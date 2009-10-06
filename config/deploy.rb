9a819b792f26e45498d69adcd8cf413ee88a0b12
Philip McClure <phil@phils-laptop.(none)>
2009-10-06 22:19:01 +0100

    Added cap configuration and some small db yml changes

---
+++ config/deploy.rb
@@ -0,0 +1,27 @@
+set :application, "www.therailworld.com"
+set :repository,  "git@github.com:overture8/theRailWorld.git"
+
+set :scm, :git
+
+role :web, "www.therailworld.com"                          # Your HTTP server, Apache/etc
+role :app, "www.therailworld.com"                          # This may be the same as your `Web` server
+role :db,  "www.therailworld.com", :primary => true        # This is where Rails migrations will run
+
+before "deploy:setup", "db:password"
+
+namespace :deploy do
+  task :start {}
+  task :stop {}
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+  end
+end
+
+namespace :db do
+  desc "Create database password in shared path"
+  task :password do
+    set :db_password, Proc.new { Capistrano::CLI.password_prompt("Remote database password: ") }
+    run "mkdir -p #{shared_path}/config"
+    put db_password, "#{shared_path}/config/dbpassword"
+  end
+end
