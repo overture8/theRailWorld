cf3b684ad7685334ecb3aec2a55679c367c3491b
Philip McClure <phil@phils-laptop.(none)>
2009-10-07 21:21:06 +0100

    Added taggable plugin

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,14 +1,22 @@
+set :rails_env, "production"
+
 set :application, "www.therailworld.com"
 set :repository,  "git@github.com:overture8/theRailWorld.git"
 set :deploy_to, "/var/www/#{application}" 
 
 set :scm, :git
+ssh_options[:forward_agent] = true
+set :scm_verbose, true
 
+set :user, "root"
+set :runner, user
 role :web, "174.143.168.192"                          # Your HTTP server, Apache/etc
 role :app, "174.143.168.192"                          # This may be the same as your `Web` server
 role :db,  "174.143.168.192", :primary => true        # This is where Rails migrations will run
+set :use_sudo, false
 
 before "deploy:setup", "db:password"
+after "deploy:symlink", "files:cp_database_yml"
 
 namespace :deploy do
 #  task :start {}
@@ -21,9 +29,15 @@ end
 namespace :db do
   desc "Create database password in shared path"
   task :password do
-    run "cp #{File.join(current_path,'config','database-example.yml')} #{File.join(current_path,'config','database.yml')}"
     set :db_password, Proc.new { Capistrano::CLI.password_prompt("Remote database password: ") }
     run "mkdir -p #{shared_path}/config"
     put db_password, "#{shared_path}/config/dbpassword"
+  end
+end
+
+namespace :files do
+  desc "Copy example database file to live database file"
+  task :cp_database_yml do
+    run "cp #{File.join(current_path,'config','database-example.yml')} #{File.join(current_path,'config','database.yml')}"
   end
 end
